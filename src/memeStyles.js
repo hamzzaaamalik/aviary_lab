691d7b6ed0a4baa8f86b6b34a769051d387621df
Echo <echo@aviary.dev>
2026-07-14 14:57:36 +0500

    added memeStyles for more epic meme presentations

---
+++ src/memeStyles.js
@@ -0,0 +1,38 @@
+// memeStyles.js - define styles for memes to keep things fresh and fun
+
+const memeStyles = {
+    // classic meme format
+    classic: {
+        font: 'Impact',
+        fontSize: '26px',
+        color: '#FFFFFF',
+        textShadow: '2px 2px 0px #000000',
+        alignment: 'center'
+    },
+    // modern minimalist style
+    minimalist: {
+        font: 'Arial',
+        fontSize: '20px',
+        color: '#000000',
+        textShadow: 'none',
+        alignment: 'left'
+    },
+    // vibrant party style
+    vibrant: {
+        font: 'Comic Sans MS',
+        fontSize: '30px',
+        color: '#FF69B4',
+        textShadow: '2px 2px 2px #FFF700',
+        alignment: 'right'
+    },
+    // retro style
+    retro: {
+        font: 'Courier New',
+        fontSize: '28px',
+        color: '#6A5ACD',
+        textShadow: '1px 1px 1px #00FF7F',
+        alignment: 'center'
+    }
+};
+
+export default memeStyles;
