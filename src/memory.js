c0b2c928b749e67bfecaf17caa8ab21ef5f21188
Echo <echo@aviary.dev>
2026-07-14 14:39:05 +0500

    added memory magic for PROTO!

--- src/memory.js
+++ src/memory.js
@@ -1,28 +1,34 @@
-class MemoryManager {
-  constructor() {
-    this.storage = {};
-  }
+// src/memory.js
 
-  set(key, value) {
-    this.storage[key] = value;
-  }
+const MemoryBank = (() => {
+    const memories = {};
 
-  get(key) {
-    return this.storage[key] !== undefined ? this.storage[key] : null;
-  }
+    const addMemory = (key, value) => {
+        memories[key] = value;
+        console.log(`Memory added: ${key} -> ${value}`);
+    };
 
-  delete(key) {
-    if (this.storage[key]) {
-      delete this.storage[key];
-    }
-  }
+    const retrieveMemory = (key) => {
+        if (memories[key]) {
+            console.log(`Memory retrieved: ${key} -> ${memories[key]}`);
+            return memories[key];
+        } else {
+            console.log(`No memory found for: ${key}`);
+            return null;
+        }
+    };
 
-  clear() {
-    this.storage = {};
-  }
-}
+    const listMemories = () => {
+        console.log('Current memories:', memories);
+        return memories;
+    };
 
-const memory = new MemoryManager();
+    return {
+        addMemory,
+        retrieveMemory,
+        listMemories,
+    };
+})();
 
-// Expose memory functions for proto to use
-module.exports = memory;
+// Export MemoryBank for use in other modules
+export default MemoryBank;
