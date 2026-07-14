c8f08cfc097f98fcad556ae10bddbfd9fa5f5ee3
Vex <vex@aviary.dev>
2026-07-14 15:14:53 +0500

    refactored state management for memes

--- src/memeState.js
+++ src/memeState.js
@@ -1,41 +1,20 @@
-class MemeState {
-    constructor() {
-        this.memes = [];
-        this.currentIndex = 0;
-    }
+const initialState = {
+    memes: [],
+    loading: false,
+    error: null,
+};
 
-    addMeme(meme) {
-        this.memes.push(meme);
-        this.currentIndex = this.memes.length - 1;
+const memeStateReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'LOAD_MEMES':
+            return { ...state, loading: true, error: null };
+        case 'LOAD_MEMES_SUCCESS':
+            return { ...state, loading: false, memes: action.payload };
+        case 'LOAD_MEMES_FAILURE':
+            return { ...state, loading: false, error: action.error };
+        default:
+            return state;
     }
+};
 
-    removeMeme(index) {
-        if (index >= 0 && index < this.memes.length) {
-            this.memes.splice(index, 1);
-            this.currentIndex = Math.max(0, this.currentIndex - 1);
-        } else {
-            console.warn('index out of bounds');
-        }
-    }
-
-    getCurrentMeme() {
-        return this.memes[this.currentIndex];
-    }
-
-    nextMeme() {
-        this.currentIndex = (this.currentIndex + 1) % this.memes.length;
-        return this.getCurrentMeme();
-    }
-
-    previousMeme() {
-        this.currentIndex = (this.currentIndex - 1 + this.memes.length) % this.memes.length;
-        return this.getCurrentMeme();
-    }
-
-    getAllMemes() {
-        return this.memes;
-    }
-}
-
-// exporting the meme state for use in other modules
-export default MemeState;
+export { initialState, memeStateReducer };
