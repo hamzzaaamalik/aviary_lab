fc64df43de32a70d0f25a5ad24aa6cd47dc02622
Echo <echo@aviary.dev>
2026-07-14 14:55:34 +0500

    refactored meme generator for more meme magic

--- src/memeGenerator.js
+++ src/memeGenerator.js
@@ -1,34 +1,39 @@
+// memeGenerator.js
+// a module to create and curate memes in the lab
+
+const { generateMemeTemplate } = require('./memeFramework');
+const { fetchRandomImage } = require('./memes');
+
 class MemeGenerator {
     constructor() {
         this.templates = [];
-        this.currentMeme = null;
     }
 
-    addTemplate(template) {
-        if (this.validateTemplate(template)) {
-            this.templates.push(template);
-        } else {
-            throw new Error('Invalid template structure.');
-        }
+    // Load meme templates from the framework
+    loadTemplates() {
+        this.templates = generateMemeTemplate();
+        console.log('Loaded meme templates:', this.templates);
     }
 
-    validateTemplate(template) {
-        // Ensure template has necessary properties.
-        return template.hasOwnProperty('text') && template.hasOwnProperty('image');
+    // Generate a meme using a random image and a template
+    async createMeme() {
+        const image = await fetchRandomImage();
+        const template = this.getRandomTemplate();
+        const meme = this.applyTemplate(image, template);
+        console.log('Generated meme:', meme);
+        return meme;
     }
 
-    generateMeme() {
-        if (this.templates.length === 0) {
-            throw new Error('No templates available.');
-        }
-        const randomTemplate = this.templates[Math.floor(Math.random() * this.templates.length)];
-        this.currentMeme = {
-            text: randomTemplate.text,
-            image: randomTemplate.image,
-            createdAt: new Date()
-        };
-        return this.currentMeme;
+    // Select a random template
+    getRandomTemplate() {
+        const randomIndex = Math.floor(Math.random() * this.templates.length);
+        return this.templates[randomIndex];
+    }
+
+    // Apply template to image
+    applyTemplate(image, template) {
+        return `${template.text} - ${image.url}`;
     }
 }
 
-export default MemeGenerator;
+module.exports = new MemeGenerator();
