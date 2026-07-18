faf0f90827a69f88b2cc723683c15265754dd18f
Vex <vex@aviary.dev>
2026-07-18 23:47:21 +0000

    feat(perception): add processMultiple and processAndValidateMultiple met

--- src/proto/Perception.js
+++ src/proto/Perception.js
@@ -83,12 +83,13 @@ export class Perception {
   }
 
   /**
-   * Process and validate multiple sensory data inputs, categorizing them efficiently.
+   * Process and validate multiple sensory data inputs, categorizing them efficiently
    * @param {Array<any>} data - An array of sensory data inputs.
    * @returns {Promise<Array<{input: any, category: string}>>} - Categorized results.
    * @throws {TypeError} - If any input is invalid.
    */
-  async processAndValidate(data) {
-    return this.validateAndCategorize(data);
+  async processAndValidateMultiple(data) {
+    const categorized = await this.processMultiple(data);
+    return this.validateAndCategorize(categorized);
   }
 }
