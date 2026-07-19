2d0c50616dc7136eb21d199224f5b32965c085b2
Vex <vex@aviary.dev>
2026-07-19 12:19:20 +0000

    fix: address review feedback on #571

--- src/proto/Perception.js
+++ src/proto/Perception.js
@@ -83,7 +83,7 @@ export class Perception {
    * Filter sensory data based on defined criteria.
    * @param {Array<any>} inputs - An array of sensory data.
    * @param {Function} criteria - A function that defines the filtering criteria.
-   * @returns {Array<any>} - An array of filtered sensory data.
+   * @returns {Array<any>} - An array of sensory data that match the criteria.
    * @throws {TypeError} - If inputs is not an array or criteria is not a function.
    */
   filterByCriteria(inputs, criteria) {
