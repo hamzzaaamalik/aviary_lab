bdc5d38fb3b18c8fbfa78d3ddd1d659df81ee44b
Vex <vex@aviary.dev>
2026-07-20 19:28:26 +0000

    fix: address review feedback on #684

--- test/proto/Perception.test.js
+++ test/proto/Perception.test.js
@@ -4,26 +4,25 @@ import { Perception } from '../../src/proto/Perception.js';
 
 const perception = new Perception();
 
-// Existing tests...
-
 test('filterSensoryInputs filters inputs by category', () => {
   const inputs = [
-    { type: 'visual', data: { brightness: 70 } },
-    { type: 'auditory', data: { volume: 50 } },
-    { type: 'visual', data: { brightness: 30 } }
+    { type: 'sound', data: 'whistle' },
+    { type: 'sight', data: 'tree' },
+    { type: 'sound', data: 'clap' }
   ];
-  const filtered = perception.filterSensoryInputs(inputs, 'visual');
+  const filtered = perception.filterSensoryInputs(inputs, 'sound');
   assert.deepEqual(filtered, [
-    { type: 'visual', data: { brightness: 70 } },
-    { type: 'visual', data: { brightness: 30 } }
+    { type: 'sound', data: 'whistle' },
+    { type: 'sound', data: 'clap' }
   ]);
 });
 
 test('filterSensoryInputs throws on invalid category', () => {
   assert.throws(() => perception.filterSensoryInputs([], ''), TypeError);
-  assert.throws(() => perception.filterSensoryInputs([], ' '), TypeError);
+  assert.throws(() => perception.filterSensoryInputs([], 123), TypeError);
 });
 
-test('filterSensoryInputs throws on invalid sensory inputs', () => {
-  assert.throws(() => perception.filterSensoryInputs('not an array', 'visual'), TypeError);
+test('filterSensoryInputs throws on invalid inputs', () => {
+  assert.throws(() => perception.filterSensoryInputs(null, 'sound'), TypeError);
+  assert.throws(() => perception.filterSensoryInputs({}, 'sound'), TypeError);
 });
