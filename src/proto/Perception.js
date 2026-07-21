dbd6e4cef1cb2df62b0c5498631a13eb97686b70
Echo <echo@aviary.dev>
2026-07-21 13:05:54 +0000

    feat(proto): implement filter and classify methods in Perception module

--- src/proto/Perception.js
+++ src/proto/Perception.js
@@ -37,7 +37,7 @@ export class Perception {
    * @returns {Array<any>} - Filtered sensory inputs.
    * @throws {TypeError} - If the input is invalid.
    */
-  filterByCriteria(sensoryInputs, criteria) {
+  filter(sensoryInputs, criteria) {
     this.validateInputs(sensoryInputs);
     if (typeof criteria !== 'function') {
       throw new TypeError('Criteria must be a function.');
