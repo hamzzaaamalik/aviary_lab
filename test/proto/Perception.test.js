c338527f6e34eb5741cefa726c47c4577b45eb5c
Vex <vex@aviary.dev>
2026-07-21 01:36:32 +0000

    feat(perception): extend sensory input handling

--- test/proto/Perception.test.js
+++ test/proto/Perception.test.js
@@ -2,30 +2,53 @@ import { test } from 'node:test';
 import assert from 'node:assert/strict';
 import { Perception } from '../../src/proto/Perception.js';
 
-const perception = new Perception();
-
-test('advancedFilterSensoryInputs filters correctly by multiple categories', () => {
+test('categorize sensory inputs', () => {
+  const perception = new Perception();
   const inputs = [
-    { type: 'sound', data: 'beep' },
-    { type: 'sight', data: 'image' },
-    { type: 'sound', data: 'buzz' },
+    { type: 'sight', data: 'light' },
+    { type: 'sound', data: 'wave' },
+    { type: 'sight', data: 'color' }
   ];
-  const result = perception.advancedFilterSensoryInputs(inputs, ['sound']);
-  assert.deepEqual(result, [
-    { type: 'sound', data: 'beep' },
-    { type: 'sound', data: 'buzz' },
+  const categorized = perception.categorizeSensoryInputs(inputs);
+  assert.deepEqual(categorized, {
+    sight: [
+      { type: 'sight', data: 'light' },
+      { type: 'sight', data: 'color' }
+    ],
+    sound: [{ type: 'sound', data: 'wave' }]
+  });
+});
+
+
+test('filter sensory inputs by category', () => {
+  const perception = new Perception();
+  const inputs = [
+    { type: 'sight', data: 'light' },
+    { type: 'sound', data: 'wave' }
+  ];
+  const filtered = perception.filterSensoryInputs(inputs, 'sight');
+  assert.deepEqual(filtered, [{ type: 'sight', data: 'light' }]);
+});
+
+
+test('advanced filter sensory inputs by categories', () => {
+  const perception = new Perception();
+  const inputs = [
+    { type: 'sight', data: 'light' },
+    { type: 'sound', data: 'wave' },
+    { type: 'sound', data: 'echo' }
+  ];
+  const filtered = perception.advancedFilterSensoryInputs(inputs, ['sound']);
+  assert.deepEqual(filtered, [
+    { type: 'sound', data: 'wave' },
+    { type: 'sound', data: 'echo' }
   ]);
 });
 
-test('advancedFilterSensoryInputs throws on invalid categories', () => {
-  const inputs = [
-    { type: 'sound', data: 'beep' },
-  ];
-  assert.throws(() => perception.advancedFilterSensoryInputs(inputs, []), TypeError);
-  assert.throws(() => perception.advancedFilterSensoryInputs(inputs, ['sound', '']), TypeError);
-});
 
-test('advancedFilterSensoryInputs validates sensory inputs', () => {
-  assert.throws(() => perception.advancedFilterSensoryInputs(null, ['sound']), TypeError);
-  assert.throws(() => perception.advancedFilterSensoryInputs([], ['sound']), TypeError);
+test('validate sensory inputs', () => {
+  const perception = new Perception();
+  assert.throws(() => perception.validateSensoryInputs([]), TypeError);
+  assert.throws(() => perception.validateSensoryInputs([{ type: 'sight' }, null]), TypeError);
+  assert.throws(() => perception.validateSensoryInputs([{ data: 'light' }]), TypeError);
 });
