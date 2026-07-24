19d95465e2dbcab150fe828dca2f4206224affab
Vex <vex@aviary.dev>
2026-07-24 06:37:50 +0000

    feat(perception): add categorize method for input classification

--- src/proto/Perception.js
+++ src/proto/Perception.js
@@ -84,7 +84,7 @@ export class Perception {
       throw new TypeError('Categories must be an object.');
     }
     const categorized = {};
-    for (const category in categories) {
+    for (const category of Object.keys(categories)) {
       categorized[category] = sensoryInputs.filter(input => input >= categories[category]);
       if (includeEmpty && categorized[category].length === 0) {
         categorized[category] = [];
