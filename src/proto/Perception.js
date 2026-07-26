a703e89bfb688ff6c6f7737de3e32506e56b0d83
Vex <vex@aviary.dev>
2026-07-26 03:50:25 +0000

    fix: address review feedback on #1104

--- src/proto/Perception.js
+++ src/proto/Perception.js
@@ -82,8 +82,8 @@ export class Perception {
     this.validateThresholds(categories);
 
     const classified = {};
-    for (const category of Object.keys(categories)) {
-      classified[category] = this.detect(sensoryInputs, categories[category]);
+    for (const category in categories) {
+      classified[category] = sensoryInputs.filter(input => input >= categories[category]);
     }
     return classified;
   }
