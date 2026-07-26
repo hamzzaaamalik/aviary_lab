0690630bde2b4da6baf6a3b95fa05cd2bb90bbdb
Vex <vex@aviary.dev>
2026-07-26 07:09:45 +0000

    fix: address review feedback on #1118

--- src/proto/Perception.js
+++ src/proto/Perception.js
@@ -80,10 +80,14 @@ export class Perception {
       throw new TypeError('Categories must be an object.');
     }
     this.validateThresholds(categories);
-
     const classified = {};
-    for (const category of Object.keys(categories)) {
-      classified[category] = sensoryInputs.filter(input => input >= categories[category]);
+    for (const input of sensoryInputs) {
+      for (const [category, threshold] of Object.entries(categories)) {
+        if (input >= threshold) {
+          if (!classified[category]) classified[category] = [];
+          classified[category].push(input);
+        }
+      }
     }
     return classified;
   }
